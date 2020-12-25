94a1d8e4b17392b8f92918af45c607f0e74af1af
Bart van der Schans <bart@vanderschans.nl>
2020-12-25 01:51:28 +0100

    Cleanup

--- 18/math3.py
+++ 18/math3.py
@@ -9,42 +9,36 @@ class MyNum:
     def __init__(self, n):
         self.n = int(n)
 
-    def __add__(self, x):
+    def __truediv__(self, x):
+        return MyNum(self.n * x.n)
+
+    def __mul__(self, x):
+        # redefine minus as multiplication
         return MyNum(self.n + x.n)
 
-    def __sub__(self, x):
-        # redefine minus as multiplication
+    def __add__(self, x):
         return MyNum(self.n * x.n)
 
 
-class MyNum2:
-    """ switch add and mul """
-
-    def __init__(self, n):
-        self.n = int(n)
-
-    def __add__(self, x):
-        return MyNum2(self.n * x.n)
-
-    def __mul__(self, x):
-        return MyNum2(self.n + x.n)
-
+# remap for precedence, overload to map back
+# equal: + => *, * => /
+# reverse: + =>v*, * => +
 
 t1 = 0
 t2 = 0
 for line in fileinput.input():
-    s1 = ""
-    s2 = ""
+    s1 = ""  # equal: + => *, * => /
+    s2 = ""  # reverse: + =>v*, * => +
     for _, c in enumerate(line.strip()):
         if c in "01234567890":
             s1 += "MyNum(" + c + ")"
-            s2 += "MyNum2(" + c + ")"
+            s2 += "MyNum(" + c + ")"
         elif c == '*':
             # abuse minus for equal operator precendence
-            s1 += '-'
+            s1 += '/'
             s2 += '+'
         elif c == '+':
-            s1 += c
+            s1 += '*'
             s2 += '*'
         else:
             s1 += c
